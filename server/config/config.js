9a6356bcd093a5529ea2e213005413e005a6246b
ramon <marquezramon63@gmail.com>
2019-09-29 19:52:43 -0400

    Variable de Entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,13 +14,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=================================
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
 
-  //  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-    urlDB = 'mongodb+srv://monramar:pata2014@cluster0-o8m6o.mongodb.net/cafe';
+   urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
 
-//}
+}
 
 process.env.URLDB = urlDB;
 
